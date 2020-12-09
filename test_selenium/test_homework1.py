bc0bb24cf83390d58c8c6fc1f7b777c162eabaa1
DANGkky <52782442+DANGkky@users.noreply.github.com>
2020-12-09 20:34:03 +0800

    1209 selenium 录制 编写 显式和隐式等待

--- test_selenium/test_homework1.py
+++ test_selenium/test_homework1.py
@@ -14,7 +14,17 @@ class TestSeleniumWork():
         self.driver.implicitly_wait(5)  # ！！！隐式等待  可以解决元素已经加载出来，但是因为dom同步等各种问题而不可点击的问题，会在设定时间内不断刷新尝试点击
 
     def teardown(self):
-        pass
+        sleep(3)
+        self.driver.quit()
+
+    def wait(self, timeout, method):
+        '''
+        简单封装显式等待函数
+        :param timeout: 显式等待时间
+        :param method: 输入搜索依据，以及定位符 比如 By.CSS_SELECTOR, "[data-name=\"求职面试圈\"]"
+        :return:
+        '''
+        WebDriverWait(self.driver, timeout).until(method)
 
     def test_open_hogwarts(self):
         '''
@@ -22,10 +32,13 @@ class TestSeleniumWork():
         '''
         self.driver.set_window_size(2000, 1000)
         self.driver.find_element(By.LINK_TEXT, "社团").click()
-        element = (By.CSS_SELECTOR, "[data-name=\"求职面试圈\"]")
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(element))
+        element1 = (By.CSS_SELECTOR, "[data-name=\"求职面试圈\"]")
+        self.wait(10, EC.element_to_be_clickable(element1))
+        # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(element))
         # self.driver.find_element_by_css_selector("#main-nav-menu > ul > li:nth-child(4) > a").click()
         # sleep(2)  # ！！！固定等待  可以解决元素还没有加载出来就尝试寻找并点击的问题，不太推荐
-        self.driver.find_element(*element).click()
-        sleep(2)
-        self.driver.find_element(By.CSS_SELECTOR, ".topic:nth-child(1) .title a").click()
+        self.driver.find_element(*element1).click()
+        element2 = (By.CSS_SELECTOR, ".topic:nth-child(1) .title a")
+        self.wait(5, EC.element_to_be_clickable(element2))
+        # self.driver.find_element(By.CSS_SELECTOR, ".topic:nth-child(1) .title a").click()
+        self.driver.find_element(*element2).click()
---
+++ test_selenium/test_homework2.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Test_Homework2():
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://testerhome.com")
+
+    def teardown(self):
+        sleep(3)
+        self.driver.quit()
+
+    def test_homework(self):
+        self.driver.find_element(By.PARTIAL_LINK_TEXT,"中国软件测试行业问卷调研").click()
