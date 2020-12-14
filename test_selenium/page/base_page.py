cab0cc3cf376f3ec910fe1a40d81609da9bad4ed
DANGkky <52782442+DANGkky@users.noreply.github.com>
2020-12-14 22:11:42 +0800

    1214 代码重构，加入basepage

---
+++ test_selenium/page/base_page.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+
+class BasePage:
+    def __init__(self, driver=None):
+        if driver is None:
+            self.driver = webdriver.Chrome()
+            self.driver.implicitly_wait(3)
+            self.driver.get("https://work.weixin.qq.com")
+
+            # self.driver.get(self._base_url)
+        else:
+            self.driver = driver
--- test_selenium/testcase/test_index.py
+++ test_selenium/testcase/test_index.py
@@ -1,19 +1,14 @@
-from selenium import webdriver
-
 from test_selenium.page.index import Index
 
 
 class TestIndex():
 
     def setup(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(3)
-        self.driver.get('https://work.weixin.qq.com/')
+        self.index = Index()
 
     def test_register(self):
         # self.driver.find_element(By.PARTIAL_LINK_TEXT, '立即注册').click()  # 点击立即注册
         # self.driver.find_element(By.ID, 'corp_name').send_keys('小游戏测试')
         # self.driver.find_element(By.ID, 'iagree').click()
         # self.driver.find_element(By.ID, 'submit_btn').click()
-        index = Index(self.driver)
-        index.goto_register().register('小游戏测试组')
+        self.index.goto_register().register('小游戏测试组')
